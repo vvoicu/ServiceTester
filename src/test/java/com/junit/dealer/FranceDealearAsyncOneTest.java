d8bdf705942a72e75e17fb4d4192e5ec7d6d426a
Voicu Vlad <voicu.vac@evozon.com>
2014-04-29 15:41:42 +0300

    removed comments and unused methods from test

--- src/test/java/com/junit/dealer/FranceDealearAsyncOneTest.java
+++ src/test/java/com/junit/dealer/FranceDealearAsyncOneTest.java
@@ -1,7 +1,6 @@
 package com.junit.dealer;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,21 +12,15 @@ import com.helios.services.dealers.france.datamodels.DealerModel;
 import com.helios.tools.utils.FormatterUtils;
 import com.junit.BaseTest;
 
-import cucumber.api.java.After;
-
 @RunWith(JUnit4.class)
 public class FranceDealearAsyncOneTest extends BaseTest {
 
-	public String url = "http://localhost:8081";
+	public String url = "http://localhost:8888";
 	public DealerModel dModel;
 	public FranceDealerFormatter formatter = new FranceDealerFormatter();
 
 	@Before
 	public void dataSetup() throws Exception {
-
-		// System.out.println(mongoUtilus.hasEntries(this.getClass().getSimpleName()));
-		// System.out.println(this.getClass().getSimpleName());
-		// System.out.println(mongoUtilus.hasEntries(this.getClass().getSimpleName()));
 
 		if (mongoQA.hasEntries(this.getClass().getSimpleName()) > 0) {
 			dModel = MongoQAConnector.getFranceDealear(this.getClass().getSimpleName());
@@ -43,37 +36,27 @@ public class FranceDealearAsyncOneTest extends BaseTest {
 	public void testDataPersistanceRule() throws Exception {
 
 		String myMessage = FormatterUtils.printResutls(formatter.generateFranceData(dModel));
-		// docList.add(formatter.generateFranceData(dModel));
-		// String myMessage =
-		// FormatterUtils.printResutls(formatter.generateDocumentRoot(docList));
 
 		HttpResponse responseMessage = protocol.sendGet(url, myMessage);
 
-		// StringEntity result = new StringEntity(responseMessage.toString(),
-		// "UTF8");
-		@SuppressWarnings("deprecation")
-		String result = EntityUtils.getContentCharSet(responseMessage.getEntity());
+//		@SuppressWarnings("deprecation")
+//		String result = EntityUtils.getContentCharSet(responseMessage.getEntity());
 
-		// validatorUtils.printMessage(responseMessage);
+		validatorUtils.printMessage(responseMessage);
 
-		System.out.println("Response message: " + result);
+//		 System.out.println("Response message: " + result);
 		// System.out.println(EntityUtils.toString(responseMessage.getEntity()).toString());
 
+		System.out.println("Read from Dev mongo reads: ");
 		mongoDev.displayAllDataSets();
 		mongoDev.displayCollectionNames();
 
+		System.out.println("Model display: ");
 		System.out.println(dModel.getDealer_name());
 
 		DealerModel qaDealerModel = MongoQAConnector.getFranceDealear(this.getClass().getSimpleName());
 
 		System.out.println("Read from QA mongo: bir -> " + qaDealerModel.getBir_id());
-	}
-
-	@After
-	public void saveToQaData() {
-		// MongoQAConnector.saveFranceDealer(dModel,
-		// this.getClass().getSimpleName());
 
 	}
-
 }
